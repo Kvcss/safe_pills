5d35f887edba55fd17f351c5d1617d058ebf72c9
Kvcss <kaiovsilva60@gmail.com>
2025-01-23 22:12:15 -0300

    feat: start the app

---
+++ android/app/src/main/kotlin/com/example/safe_pills/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.safe_pills
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+Future<List> getPeople ()async{
+    List people = [];
+
+    return people; 
+}
